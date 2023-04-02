55de62bec427ac78ccbd6e0e8d7043412c1dbf2c
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-04-02 23:30:45 +0200

    nit: reset input select timeout to 50ms

--- src/components/nodes/LabelUpdaterNode.tsx
+++ src/components/nodes/LabelUpdaterNode.tsx
@@ -29,7 +29,7 @@ export function LabelUpdaterNode({
 
     // Have to do this with a bit of a delay to
     // ensure it works when triggered via navbar.
-    setTimeout(() => input?.select(), 0);
+    setTimeout(() => input?.select(), 50);
   }, []);
 
   const cancel = () => {

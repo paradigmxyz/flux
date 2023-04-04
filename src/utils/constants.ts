982d8ed9d4c054e3b2e3c642d1412e63dda20560
ghgoodreau <ghgoodreau@protonmail.com>
2023-04-04 16:17:39 -0500

    nit: constant organization

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -45,6 +45,7 @@ export const OVERLAP_RANDOMNESS_MAX = 20;
 export const API_KEY_LOCAL_STORAGE_KEY = "FLUX_OPENAI_API_KEY";
 export const REACT_FLOW_LOCAL_STORAGE_KEY = "FLUX_REACT_FLOW_DATA";
 export const MODEL_SETTINGS_LOCAL_STORAGE_KEY = "FLUX_MODEL_SETTINGS";
+export const SAVED_CHAT_SIZE_LOCAL_STORAGE_KEY = "FLUX_SAVED_CHAT_SIZE";
 
 export const NEW_TREE_CONTENT_QUERY_PARAM = "newTreeWith";
 
@@ -60,8 +61,6 @@ export const CODE_BLOCK_LANGUAGE_DETECT_REGEX = /^```[a-zA-Z0-9-]*$/m;
 
 export const STREAM_CANCELED_ERROR_MESSAGE = "STREAM_CANCELED";
 export const STALE_STREAM_ERROR_MESSAGE = "STALE_STREAM";
-
-export const SAVED_CHAT_SIZE_LOCAL_STORAGE_KEY = "FLUX_SAVED_CHAT_SIZE";
 
 // Magic number to almost always make auto-label text stay in two lines.
 export const MAX_AUTOLABEL_CHARS = 32;

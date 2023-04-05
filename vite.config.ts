7e7aca12e936e10eaef7268caceefa51be1a9620
Vladyslav Dalechyn <vlad.dalechin@gmail.com>
2023-04-05 21:54:17 +0300

    ci: bundle diff report

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      emitFile: true,
+      template: "raw-data",
+    })
+  ],
 });

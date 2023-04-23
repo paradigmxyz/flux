bb055d1356c666360b27de8c3a970bafed280fb5
Barton Rhodes <barton@bmorphism.xyz>
2023-04-23 19:26:46 +0000

    gm

---
+++ devenv.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    npm run dev
+  '';
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+   languages.typescript.enable = true;
+   languages.javascript.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}

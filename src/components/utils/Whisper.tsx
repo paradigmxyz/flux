a504d70194be47b4b2064a8abdc26efd17c59b55
ghgoodreau <ghgoodreau@protonmail.com>
2023-05-04 17:27:56 -0500

    some progress

--- src/components/utils/Whisper.tsx
+++ src/components/utils/Whisper.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Button, Flex } from "@chakra-ui/react";
+import { Button, Box, Spinner, Tooltip } from "@chakra-ui/react";
 import { FaMicrophone, FaMicrophoneSlash } from "react-icons/fa";
 
 export const Whisper = ({
@@ -9,10 +9,10 @@ export const Whisper = ({
   onConvertedText: (text: string) => void;
   apiKey: string | null;
 }) => {
-  const [isRecording, setIsRecording] = useState<boolean>(false);
-  const [audioData, setAudioData] = useState<Blob | null>(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [isTranscribing, setIsTranscribing] = useState(false);
   const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
-  const [hasRecordingSupport, setHasRecordingSupport] = useState<boolean>(false);
+  const [hasRecordingSupport, setHasRecordingSupport] = useState(false);
 
   const checkMediaRecordingSupport = async () => {
     if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
@@ -33,16 +33,32 @@ export const Whisper = ({
     checkMediaRecordingSupport();
   }, []);
 
+  const onDataAvailable = (e: BlobEvent) => {
+    const formData = new FormData();
+    formData.append("file", e.data, "recording.webm");
+    formData.append("model", "whisper-1");
+
+    setIsTranscribing(true);
+    fetch("https://api.openai.com/v1/audio/transcriptions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => onConvertedText(data.text))
+      .catch((err) => console.error("Error:", err))
+      .finally(() => setIsTranscribing(false));
+  };
+
   const startRecording = async () => {
     setIsRecording(true);
 
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
     const recorder = new MediaRecorder(stream);
 
-    recorder.addEventListener("dataavailable", (e: BlobEvent) => {
-      setAudioData(e.data);
-      stream.getTracks().forEach((track) => track.stop());
-    });
+    recorder.addEventListener("dataavailable", onDataAvailable);
 
     recorder.start();
     setMediaRecorder(recorder);
@@ -51,67 +67,48 @@ export const Whisper = ({
   const stopRecording = () => {
     if (mediaRecorder) {
       mediaRecorder.stop();
-      setIsRecording(false);
     }
-  };
-
-  const transcribeAudio = async () => {
-    if (!audioData || !apiKey) return;
-
-    const formData = new FormData();
-    formData.append("file", audioData, `audio_${Date.now()}.webm`);
-    formData.append("model", "whisper-1");
-
-    try {
-      const response = await fetch("https://api.openai.com/v1/audio/transcriptions", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${apiKey}`,
-        },
-        body: formData,
-      });
-
-      if (!response.ok) {
-        console.error("Whisper API error:", response.statusText);
-        return;
-      }
-
-      const responseData = await response.json();
-      onConvertedText(responseData.text);
-      setAudioData(null);
-    } catch (err) {
-      console.error("Error:", err);
-    }
+    setIsRecording(false);
   };
 
   return (
     <>
       {hasRecordingSupport && (
-        <Flex mt={3}>
-          {!isRecording && (
-            <Button onClick={startRecording} leftIcon={<FaMicrophone />} mr={3}>
-              Start Recording
-            </Button>
-          )}
-
-          {isRecording && (
+        <Box mt={10}>
+          <Tooltip
+            label={
+              isRecording
+                ? "Stop Recording"
+                : isTranscribing
+                ? "Transcribing, please wait..."
+                : "Record for Transcription"
+            }
+            openDelay={500}
+            placement="top-start"
+          >
             <Button
-              onClick={stopRecording}
-              mr={3}
-              bg={"#f14249"}
-              _hover={{ bg: "#ed303f" }}
-              leftIcon={<FaMicrophoneSlash />}
+              position="absolute"
+              marginTop={4}
+              bottom={1}
+              right={1}
+              zIndex={10}
+              variant="outline"
+              border="0px"
+              _hover={{ background: "none" }}
+              p={1}
+              onClick={isRecording ? stopRecording : startRecording}
+              disabled={isTranscribing}
             >
-              Stop Recording
+              {isRecording ? (
+                <FaMicrophoneSlash />
+              ) : isTranscribing ? (
+                <Spinner size="sm" color={"#404040"} />
+              ) : (
+                <FaMicrophone />
+              )}
             </Button>
-          )}
-
-          {audioData && (
-            <Button onClick={transcribeAudio} colorScheme="blue">
-              Transcribe Audio
-            </Button>
-          )}
-        </Flex>
+          </Tooltip>
+        </Box>
       )}
     </>
   );

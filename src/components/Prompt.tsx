d134b347f3c44936332fd4073005aa839fc58096
hunter <ghgoodreau@protonmail.com>
2023-05-19 22:14:40 -0500

    feat: whisper support (#76)
    Co-authored-by: t11s <greenfeatherhelp@gmail.com>

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -13,6 +13,7 @@ import { useState, useEffect, useRef } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import { Node, useReactFlow } from "reactflow";
 import { getPlatformModifierKeyText } from "../utils/platform";
+import { Whisper } from "./utils/Whisper";
 
 export function Prompt({
   lineage,
@@ -23,6 +24,7 @@ export function Prompt({
   isGPT4,
   settings,
   setSettings,
+  apiKey,
 }: {
   lineage: Node<FluxNodeData>[];
   onType: (text: string) => void;
@@ -32,6 +34,7 @@ export function Prompt({
   isGPT4: boolean;
   settings: Settings;
   setSettings: (settings: Settings) => void;
+  apiKey: string | null;
 }) {
   const { setNodes } = useReactFlow();
 
@@ -179,8 +182,8 @@ export function Prompt({
                     zIndex={10}
                     variant="outline"
                     border="0px"
-                    _hover={{ background: "none" }}
                     p={1}
+                    _hover={{ background: "none" }}
                   >
                     {data.streamId ? (
                       <NotAllowedIcon boxSize={4} />
@@ -202,25 +205,41 @@ export function Prompt({
                     crossAxisAlignment="flex-start"
                     borderRadius="6px"
                     wordBreak="break-word"
+                    minHeight={
+                      data.fluxNodeType === FluxNodeType.User && isLast && isEditing
+                        ? "75px"
+                        : "0px"
+                    }
                   >
                     {isLast && isEditing ? (
-                      <TextareaAutosize
-                        id="promptBox"
-                        style={{
-                          width: "100%",
-                          backgroundColor: "transparent",
-                          outline: "none",
-                        }}
-                        value={data.text ?? ""}
-                        onChange={(e) => onType(e.target.value)}
-                        placeholder={
-                          data.fluxNodeType === FluxNodeType.User
-                            ? "Write a poem about..."
-                            : data.fluxNodeType === FluxNodeType.System
-                            ? "You are ChatGPT..."
-                            : undefined
-                        }
-                      />
+                      <>
+                        <TextareaAutosize
+                          id="promptBox"
+                          style={{
+                            width: "100%",
+                            backgroundColor: "transparent",
+                            outline: "none",
+                          }}
+                          minRows={data.fluxNodeType === FluxNodeType.User ? 3 : 1}
+                          value={data.text ?? ""}
+                          onChange={(e) => onType(e.target.value)}
+                          placeholder={
+                            data.fluxNodeType === FluxNodeType.User
+                              ? "Write a poem about..."
+                              : data.fluxNodeType === FluxNodeType.System
+                              ? "You are ChatGPT..."
+                              : undefined
+                          }
+                        />
+                        {data.fluxNodeType === FluxNodeType.User && (
+                          <Whisper
+                            onConvertedText={(text: string) =>
+                              onType(`${data.text}${data.text ? " " : ""}${text}`)
+                            }
+                            apiKey={apiKey}
+                          />
+                        )}
+                      </>
                     ) : (
                       <Markdown text={data.text} />
                     )}

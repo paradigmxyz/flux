3a0a8fc3ae731593cba19a8e93efb0f01092ac8b
Ansgar Dietrichs <adietrichs@gmail.com>
2023-06-14 03:04:09 +0200

    fetch models on startup and key change

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -45,6 +45,7 @@ import {
 } from "../utils/fluxNode";
 import { useLocalStorage } from "../utils/lstore";
 import { mod } from "../utils/mod";
+import { getAvailableChatModels } from "../utils/models";
 import { generateNodeId, generateStreamId } from "../utils/nodeId";
 import { messagesFromLineage, promptFromLineage } from "../utils/prompt";
 import { getQueryParam, resetURL } from "../utils/qparams";
@@ -866,6 +867,46 @@ function App() {
   const [apiKey, setApiKey] = useLocalStorage<string>(API_KEY_LOCAL_STORAGE_KEY);
 
   const [availableModels, setAvailableModels] = useState<string[] | null>(SUPPORTED_MODELS);
+  const modelsLoadCounter = useRef(0);
+
+  // Load available models for the API key.
+  useEffect(() => {
+    if (isValidAPIKey(apiKey)) {
+      const modelsLoadIndex = modelsLoadCounter.current + 1;
+      modelsLoadCounter.current = modelsLoadIndex;
+      setAvailableModels(null);
+
+      (async () => {
+        let modelList: string[] = [];
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        try {
+          modelList = await getAvailableChatModels(apiKey!);
+        } catch (e) {
+          toast({
+            title: "Failed to load model list!",
+            status: "error",
+            ...TOAST_CONFIG,
+          });
+        }
+        if (modelsLoadIndex !== modelsLoadCounter.current) return;
+
+        if (modelList.length === 0) modelList.push(settings.model);
+        setAvailableModels(modelList);
+
+        if (!modelList.includes(settings.model)) {
+          const oldModel = settings.model;
+          const newModel = modelList.includes(DEFAULT_SETTINGS.model) ? DEFAULT_SETTINGS.model : modelList[0];
+          setSettings((settings) => ({ ...settings, model: newModel }));
+          toast({
+            title: `Model "${oldModel}" no longer available!`,
+            description: `Switched to "${newModel}"`,
+            status: "warning",
+            ...TOAST_CONFIG,
+          });
+        }
+      })();
+    }
+  }, [apiKey]);
 
   const isAnythingLoading = isSavingReactFlow || isSavingSettings;
 

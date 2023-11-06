70ea1225119e8c9c9e74e83caa217a7e3cf955b3
ghgoodreau <ghgoodreau@protonmail.com>
2023-11-06 14:37:22 -0600

    add 128k gpt-4 turbo

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -15,7 +15,12 @@ export const REACT_FLOW_NODE_TYPES: Record<
   LabelUpdater: LabelUpdaterNode,
 };
 
-export const SUPPORTED_MODELS = ["gpt-3.5-turbo", "gpt-4", "gpt-4-32k"];
+export const SUPPORTED_MODELS = [
+  "gpt-3.5-turbo",
+  "gpt-4",
+  "gpt-4-32k",
+  "gpt-4-1106-preview",
+];
 
 export const DEFAULT_SETTINGS: Settings = {
   temp: 1.2,

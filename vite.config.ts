466a719a0103083c3c683707297cef671e7463cf
t11s <greenfeatherhelp@gmail.com>
2024-06-10 22:10:08 -0700

    fix: nodePolyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
+      include: ["stream"],
+    }),
+  ],
 });
